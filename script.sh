2315b4dfd5d6d51edf6f990c93ffa0b43e5e6ebf
mavrick202 <mavrick202@gmail.com>
2020-02-19 02:17:53 +0000

    Added Comment at last

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #This will give all nums from 1 to 100
-for i  in {1..100}
-do
-if  [ $(expr $i % 2) -eq 0 ]
-then
-echo "$i is a EVEN Number"
-else
-echo "$i is a ODD Numner except if it is zero"
-fi
+for i in {1..100}; do
+    if [ $(expr $i % 2) -eq 0 ]; then
+        echo "$i is a EVEN Number"
+    else
+        echo "$i is a ODD Numner except if it is zero"
+    fi
 done
+#End the script...!!!
