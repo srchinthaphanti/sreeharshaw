c9d4ad438dadee206e40d6734710fc175b364f8f
mavricktrainings <sreeharshav@mavricktrainings.com>
2020-02-19 02:36:04 +0000

    AddedWindow

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#This will give all nums from 1 to 100
+#This will give all nums from 1 to 100 By Windows User...!!
 for i in {1..100}; do
     if [ $(expr $i % 2) -eq 0 ]; then
         echo "$i is a EVEN Number"
